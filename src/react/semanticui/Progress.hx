4341da2b6a591b4710b4a4f2b3f6a374b102a4df
Kevin Leung <kevinresol@gmail.com>
2019-07-03 17:34:38 +0800

    Add Progress

---
+++ src/react/semanticui/Progress.hx
@@ -0,0 +1,27 @@
+package react.semanticui;
+
+import react.ReactComponent;
+import react.BaseProps;
+import haxe.extern.EitherType;
+import haxe.Constraints;
+
+#if react_global @:native('semanticUIReact.Progress')
+#else @:jsRequire('semantic-ui-react', 'Progress')
+#end
+extern class Progress extends ReactComponentOfProps<ProgressProps> {}
+
+typedef ProgressProps = {
+	> BasePropsWithOptChildren,
+	> BaseSemanticProps,
+	?attached:String,
+	?autoSuccess:Bool,
+	?color:String,
+	?disabled:Bool,
+	?indicating:Bool,
+	?inverted:Bool,
+	?percent:Float,
+	?size:String,
+	?success:Bool,
+	?warning:Bool,
+}
+
